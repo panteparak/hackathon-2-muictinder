f7c01d0c73b6f73119a595fa9b98d8063169da29
Pan Teparak <panteparak@me.com>
2018-05-29 11:37:22 +0700

    Add dependencies

--- App.js
+++ App.js
@@ -12,6 +12,11 @@ import {
   View
 } from 'react-native';
 
+
+import firebase from 'react-native-firebase'
+
+
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu LOL',
